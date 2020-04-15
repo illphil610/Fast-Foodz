52d14d0c08816b401b9dea0eed569dadedcb1b1b
Phil Cappelli <pcappelli901@gmail.com>
2020-04-14 20:39:06 -0400

    - fixed some minor colors defect and cleaned up codebase

--- Fast Foodz/controllers/HomeViewController.swift
+++ Fast Foodz/controllers/HomeViewController.swift
@@ -15,27 +15,47 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var listViewController: UIView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: UIControl.State.selected)
+        setupSegmentedControlTextAttributes()
     }
+    
+    // MARK: Actions
     
     @IBAction func switchViewAction(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
-               case 0:
-                   UIView.animate(withDuration: 0.5, animations: {
-                       self.mapViewController.alpha = 1
-                       self.listViewController.alpha = 0
-                   })
-               case 1:
-                   UIView.animate(withDuration: 0.5, animations: {
-                       self.mapViewController.alpha = 0
-                       self.listViewController.alpha = 1
-                   })
-               default: break
-               }
-        
+        case 0:
+            UIView.animate(
+                withDuration: 0.5, animations: {
+                    self.mapViewController.alpha = 1
+                    self.listViewController.alpha = 0
+                }
+            )
+        case 1:
+            UIView.animate(
+                withDuration: 0.5, animations: {
+                    self.mapViewController.alpha = 0
+                    self.listViewController.alpha = 1
+                }
+            )
+        default: break
+        }
     }
 
+}
+
+fileprivate extension HomeViewController {
+    
+    func setupSegmentedControlTextAttributes() {
+        segmentedControl.setTitleTextAttributes(
+            [NSAttributedString.Key.foregroundColor: UIColor.white], for: UIControl.State.selected
+        )
+        segmentedControl.setTitleTextAttributes(
+            [NSAttributedString.Key.foregroundColor: UIColor.deepIndigo], for: UIControl.State.normal
+        )
+    }
+    
 }
